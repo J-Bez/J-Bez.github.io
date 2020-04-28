be35df9c6f6a63f0dddd8147abd7221b19892e5b
Jaka Bezovšek <jb5442@student.uni-lj.si>
2020-04-28 20:45:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ function closeNav() {
 function startAnimation() {
 	console.log('PLAY ANIMATION')
 }
+
+AFRAME.registerComponent('drag-rotate-component',{
+      schema : { speed : {default:1}},
+      init : function(){
+        this.ifMouseDown = false;
+        this.x_cord = 0;
+        this.y_cord = 0;
+        document.addEventListener('touchstart',this.OnDocumentMouseDown.bind(this));
+        document.addEventListener('touchend',this.OnDocumentMouseUp.bind(this));
+        document.addEventListener('touchmove',this.OnDocumentMouseMove.bind(this));
+      },
+      OnDocumentMouseDown : function(event){
+        this.ifMouseDown = true;
+        this.x_cord = event.touches[0].pageX;
+        this.y_cord = event.touches[0].pageY;
+      },
+      OnDocumentMouseUp : function(){
+	      
+        this.ifMouseDown = false;
+	      
+      },
+      OnDocumentMouseMove : function(event)
+      {
+        if(this.ifMouseDown)
+        {
+		
+          var temp_x = event.touches[0].pageX-this.x_cord;
+          var temp_y = event.touches[0].pageY-this.y_cord;
+          if(Math.abs(temp_y)<Math.abs(temp_x))
+          {
+            this.el.object3D.rotateY(temp_x*this.data.speed/100);
+		  
+          }
+          else
+          {
+            this.el.object3D.rotateX(temp_y*this.data.speed/100);
+          }
+          this.x_cord = event.touches[0].pageX;
+          this.y_cord = event.touches[0].pageY;
+        }
+      }
+    });
